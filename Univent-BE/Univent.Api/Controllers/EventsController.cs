42741338f848470df831a0fe3a90d0f06da0c033
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2022-11-23 00:03:40 +0200

    Added Models for Events and Types of Events, along with Event Controller

---
+++ Univent-BE/Univent.Api/Controllers/EventsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Univent.Domain.Models;
+
+namespace Univent.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var getEvent = new Event { Id = id, Description = "Hello World!" };
+            return Ok(getEvent);
+        }
+    }
+}
---
+++ Univent-BE/Univent.Domain/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univent.Domain.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        //public string nameofevent { get; set; }
+        //public int maximumparticipants { get; set; }
+        //public datetime startdatetime { get; set; }
+        //public datetime enddatetime { get; set; }
+        public string Description { get; set; }
+        public int EventTypeId { get; set; }
+        public EventType EventType { get; set; }
+    }
+}
