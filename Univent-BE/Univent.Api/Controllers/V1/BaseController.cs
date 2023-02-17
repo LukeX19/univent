508bceb2044e10a69e0d4bca18c41a5ea1b4ba46
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-02-17 19:13:25 +0200

    Added non-success responses for Patch method, for UserProfiles

---
+++ Univent-BE/Univent.Api/Controllers/V1/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Univent.Api.Contracts.Error;
+using Univent.Application.Enums;
+using Univent.Application.Models;
+
+namespace Univent.Api.Controllers.V1
+{
+    public class BaseController : ControllerBase
+    {
+        protected IActionResult HandleErrorResponse(List<Error> errors)
+        {
+            var apiError = new ErrorResponse();
+
+            if(errors.Any(e => e.Code == ErrorCodeEnum.NotFound))
+            {
+                var error = errors.FirstOrDefault(e => e.Code == ErrorCodeEnum.NotFound);
+                
+                apiError.StatusCode = 404;
+                apiError.StatusMessage = "Not Found";
+                apiError.Errors.Add(error.Message);
+                apiError.Timestamp = DateTime.Now;
+
+                return NotFound(apiError);
+            }
+
+            apiError.StatusCode = 500;
+            apiError.StatusMessage = "Internal Server Error";
+            apiError.Errors.Add("Unknown Error");
+            apiError.Timestamp = DateTime.Now;
+            return StatusCode(500, apiError);
+        }
+    }
+}
---
+++ Univent-BE/Univent.Application/Models/OperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univent.Application.Models
+{
+    public class OperationResult<T>
+    {
+        //generic type for result
+        public T Payload { get; set; }
+        public bool IsError { get; set; }
+        public List<Error> Errors { get; } = new List<Error>();
+    }
+}
