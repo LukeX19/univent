6b352903b263a6dc5d542487be3d30e5feec4f40
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-02-21 18:13:54 +0200

    Updated filters

--- Univent-BE/Univent.Api/Filters/ValidateModelAttribute.cs
+++ Univent-BE/Univent.Api/Filters/ValidateModelAttribute.cs
@@ -6,24 +6,26 @@ namespace Univent.Api.Filters
 {
     public class ValidateModelAttribute : ActionFilterAttribute
     {
-        public override void OnActionExecuting(ActionExecutingContext context)
+        public override void OnResultExecuting(ResultExecutingContext context)
         {
             if(!context.ModelState.IsValid)
             {
                 var apiError = new ErrorResponse();
                 apiError.StatusCode = 400;
                 apiError.StatusMessage = "Bad Request";
-                
+                apiError.Timestamp = DateTime.Now;
+
                 var errors = context.ModelState.AsEnumerable();
 
                 foreach (var error in errors)
                 {
-                    apiError.Errors.Add(error.Value.ToString());
+                    foreach (var inner in error.Value.Errors)
+                    {
+                        apiError.Errors.Add(inner.ErrorMessage);
+                    }
                 }
 
-                apiError.Timestamp = DateTime.Now;
-
-                context.Result = new JsonResult(apiError) { StatusCode = 400 };
+                context.Result = new BadRequestObjectResult(apiError);
                 //TO DO: find a way to make sure that ASP.NET Core does
                 //not override our action result body
             }
