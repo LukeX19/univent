9b90537b6d545f725944e007db4337042458267e
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-17 14:26:55 +0300

    Added endpoint to return average rating value for a user

---
+++ api-server/Univent.Application/Ratings/Queries/GetAverageRatingByUserId.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Univent.Application.Ratings.Queries
+{
+    public class GetAverageRatingByUserId : IRequest<double>
+    {
+        public Guid UserProfileID { get; set; }
+    }
+}
