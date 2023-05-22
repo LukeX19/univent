194b1aad9d8059ec72e6a33b666a39178f5f650a
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-22 17:56:13 +0300

    Update User Profile now also updates the email in the Identity table

--- api-server/Univent.Application/UserProfiles/CommandHandlers/UpdateUserProfileBasicInformationHandler.cs
+++ api-server/Univent.Application/UserProfiles/CommandHandlers/UpdateUserProfileBasicInformationHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Univent.Application.Exceptions;
 using Univent.Application.UserProfiles.Commands;
@@ -10,24 +11,37 @@ namespace Univent.Application.UserProfiles.CommandHandlers
     internal class UpdateUserProfileBasicInformationHandler : IRequestHandler<UpdateUserProfileBasicInformationCommand>
     {
         private readonly DataContext _dbcontext;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public UpdateUserProfileBasicInformationHandler(DataContext dbcontext)
+        public UpdateUserProfileBasicInformationHandler(DataContext dbcontext, UserManager<IdentityUser> userManager)
         {
             _dbcontext = dbcontext;
+            _userManager = userManager;
         }
 
         public async Task<Unit> Handle(UpdateUserProfileBasicInformationCommand request, CancellationToken cancellationToken)
         {
             var userProfile = await _dbcontext.UserProfiles.FirstOrDefaultAsync(up => up.UserProfileID == request.UserProfileID, cancellationToken)
                 ?? throw new ObjectNotFoundException(nameof(UserProfile), request.UserProfileID);
+
             var basicInformation = BasicInformation.CreateBasicInformation(request.FirstName, request.LastName, request.EmailAddress,
                 request.PhoneNumber, request.DateOfBirth, request.Hometown, request.ProfilePicture);
+
+            var identityUser = await _userManager.FindByEmailAsync(userProfile.BasicInfo.EmailAddress);
+            if (identityUser == null)
+            {
+                throw new IdentityUserNotFoundException(request.EmailAddress);
+            }
 
             userProfile.UpdateBasicInformation(basicInformation);
             userProfile.UpdateUniversityInformation(request.UniversityID, request.Year);
 
             _dbcontext.UserProfiles.Update(userProfile);
             await _dbcontext.SaveChangesAsync(cancellationToken);
+
+            identityUser.UserName = request.EmailAddress;
+            identityUser.Email = request.EmailAddress;
+            await _userManager.UpdateAsync(identityUser);
 
             return new Unit();
         }
