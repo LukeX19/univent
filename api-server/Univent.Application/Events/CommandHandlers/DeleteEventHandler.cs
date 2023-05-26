f95b941b520f0f6f1711eedba91cd35d564351f3
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-26 23:30:19 +0300

    Admin can now delete events

--- api-server/Univent.Application/Events/CommandHandlers/DeleteEventHandler.cs
+++ api-server/Univent.Application/Events/CommandHandlers/DeleteEventHandler.cs
@@ -1,19 +1,23 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Univent.Application.Events.Commands;
 using Univent.Application.Exceptions;
 using Univent.Dal;
 using Univent.Domain.Aggregates.EventAggregate;
+using Univent.Domain.Aggregates.UserAggregate;
 
 namespace Univent.Application.Events.CommandHandlers
 {
     internal class DeleteEventHandler : IRequestHandler<DeleteEventCommand>
     {
         private readonly DataContext _dbcontext;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public DeleteEventHandler(DataContext dbcontext)
+        public DeleteEventHandler(DataContext dbcontext, UserManager<IdentityUser> userManager)
         {
             _dbcontext = dbcontext;
+            _userManager = userManager;
         }
 
         public async Task<Unit> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
@@ -21,7 +25,12 @@ namespace Univent.Application.Events.CommandHandlers
             var _event = await _dbcontext.Events.FirstOrDefaultAsync(e => e.EventID == request.EventID, cancellationToken)
                 ?? throw new ObjectNotFoundException(nameof(Event), request.EventID);
 
-            if (_event.UserProfileID != request.UserProfileID)
+            var userProfile = await _dbcontext.UserProfiles.FirstOrDefaultAsync(up => up.UserProfileID == request.UserProfileID, cancellationToken)
+                ?? throw new ObjectNotFoundException(nameof(UserProfile), request.UserProfileID);
+            var identity = await _userManager.FindByIdAsync(userProfile.IdentityID);
+            var userRole = await _userManager.GetRolesAsync(identity);
+
+            if (_event.UserProfileID != request.UserProfileID && !userRole.Contains("Admin"))
             {
                 throw new EventDeleteNotPossibleException();
             }
