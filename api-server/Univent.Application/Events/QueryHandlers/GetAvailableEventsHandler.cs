907a24e7c42319854783513a81b52a7948313bae
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2023-05-29 23:56:12 +0300

    Added endpoint to get available events (not expired and not cancelled)

---
+++ api-server/Univent.Application/Events/QueryHandlers/GetAvailableEventsHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Univent.Application.Events.Queries;
+using Univent.Dal;
+using Univent.Domain.Aggregates.EventAggregate;
+
+namespace Univent.Application.Events.QueryHandlers
+{
+    internal class GetAvailableEventsHandler : IRequestHandler<GetAvailableEvents, IEnumerable<Event>>
+    {
+        private readonly DataContext _dbcontext;
+
+        public GetAvailableEventsHandler(DataContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<IEnumerable<Event>> Handle(GetAvailableEvents request, CancellationToken cancellationToken)
+        {
+            return await _dbcontext.Events
+                .Where(e => e.EndTime > DateTime.Now && e.IsCancelled == false)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
